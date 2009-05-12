810250cb0b5534dbd559972a9e3d01132b9e042f
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-05-12 13:28:55 +0000

    changes

--- local-eggs/nva.sliteauth/nva/sliteauth/tests.py
+++
@@ -1,9 +0,0 @@
-import z3c.testsetup
-import nva.sliteauth
-
-print "This file is picked up by the testrunner..."
-
-test_suite = z3c.testsetup.register_all_tests('nva.sliteauth')
-
-#suite = test_suite()
-#get_basenames_from_suite(suite)
