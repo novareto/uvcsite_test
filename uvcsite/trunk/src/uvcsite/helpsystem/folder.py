df0fab940f339fd4883054d496d8fea502ca2e97
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-06-18 09:34:01 +0000

    Helpsystem cleanup fixes

--- uvcsite/trunk/src/uvcsite/helpsystem/folder.py
+++ uvcsite/trunk/src/uvcsite/helpsystem/folder.py
@@ -1,16 +1,13 @@
 import grok
+import megrok.pagelet
+
 from uvcsite.helpsystem.interfaces import IHelpFolder
 from zope.app.authentication.interfaces import IPrincipal
 
 class HelpFolder(grok.Container):
     grok.implements(IHelpFolder)
 
-    def traverse(self, name):
-	if not name in self:
-	    return self['keineHilfe'] 
-        return self[name]
-
-class HelpFolderIndex(grok.View):
+class HelpFolderIndex(megrok.pagelet.Pagelet):
     grok.name('index')
     pass
 
--- uvcsite/trunk/src/uvcsite/helpsystem/page.py
+++ uvcsite/trunk/src/uvcsite/helpsystem/page.py
@@ -1,19 +1,19 @@
 import grok
+from megrok.pagelet.component import FormPageletMixin
 from uvcsite.helpsystem.interfaces import IHelpFolder, IHelpPage
 
 
 class HelpPage(grok.Model):
     grok.implements(IHelpPage)
 
-    def __init__(self, name, title="", text=""):
+    def __init__(self, name="", title="", text=""):
 	self.name = name
 	self.title = title
 	self.text = text
 
-class HelpAdd(grok.AddForm):
+class HelpAdd(FormPageletMixin, grok.AddForm):
     grok.context(IHelpFolder)
     form_fields = grok.Fields(IHelpPage)
-    template = grok.PageTemplateFile('templates/form.pt')
 
     @grok.action(u"Anlegen")
     def handle_add(self, **data):
@@ -23,11 +23,10 @@ class HelpAdd(grok.AddForm):
 	container[data.get('title')] = helppage
 	self.redirect(self.url(helppage, 'overview'))
 
-class HelpPageIndex(grok.DisplayForm):
+class HelpPageIndex(FormPageletMixin,grok.DisplayForm):
     grok.name('overview')
     grok.context(IHelpPage)
     form_fields = grok.Fields(IHelpPage)
-    template = grok.PageTemplateFile('templates/display_form.pt')
 
 
 class TTDisplay(grok.View):
