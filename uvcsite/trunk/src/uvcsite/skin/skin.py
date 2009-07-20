067851bf3b912161881161bdf342c84ae903e0a3
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-07-20 10:13:52 +0000

    Working on Layout and HelpSystem

--- uvcsite/trunk/src/uvcsite/skin/skin.py
+++ uvcsite/trunk/src/uvcsite/skin/skin.py
@@ -3,10 +3,10 @@ import megrok.layout
 import megrok.resourcelibrary
 
 from zope.interface import Interface
-from megrok.z3cform.skin import FormLayer
+from megrok.z3cform.skin import TableLayer
 
 
-class IUVCSiteLayer(FormLayer):
+class IUVCSiteLayer(TableLayer):
     pass
 
 
@@ -18,13 +18,13 @@ class Resources(megrok.resourcelibrary.ResourceLibrary):
     """ Resourcen for uvcsite"""
     grok.name('uvcresources')
     grok.layer(IUVCSiteLayer)
-    #grok.require('uvc.Member')
     megrok.resourcelibrary.directory('resources')
-    megrok.resourcelibrary.include('worldcookery.css')
     megrok.resourcelibrary.include('jquery-1.3.2.js')
     megrok.resourcelibrary.include('jtip.js')
     megrok.resourcelibrary.include('myjs.js')
     megrok.resourcelibrary.include('tooltip.css')
+    megrok.resourcelibrary.include('tableform.css')
+    megrok.resourcelibrary.include('worldcookery.css')
 
 
 class StandardLayout(megrok.layout.Layout):
