b381bb8fb18f88639946ccb1a6a978cb8f3bea1f
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-07-20 12:49:31 +0000

    Working on Help Page

--- src/uvcsite/helpsystem/page.py
+++ src/uvcsite/helpsystem/page.py
@@ -1,9 +1,11 @@
 import grok
+
+from uvcsite.content.base import Content
 from megrok.layout.components import Form
 from uvcsite.helpsystem.interfaces import IHelpFolder, IHelpPage
 
 
-class HelpPage(grok.Model):
+class HelpPage(Content):
     grok.implements(IHelpPage)
 
     def __init__(self, name="", title="", text=""):
@@ -11,9 +13,11 @@ class HelpPage(grok.Model):
 	self.title = title
 	self.text = text
 
+
 class HelpAdd(Form, grok.AddForm):
     grok.context(IHelpFolder)
     form_fields = grok.Fields(IHelpPage)
+    grok.require('zope.ManageSite')
 
     @grok.action(u"Anlegen")
     def handle_add(self, **data):
@@ -21,7 +25,15 @@ class HelpAdd(Form, grok.AddForm):
 	helppage = HelpPage()
 	self.applyData(helppage, **data)
 	container[data.get('title')] = helppage
+	self.flash(u'Die Hilfeseite wurde erfolgreich angelegt')
 	self.redirect(self.url(helppage, 'overview'))
+
+
+class Edit(Form, grok.EditForm):
+    grok.context(IHelpPage)
+    form_fields = grok.Fields(IHelpPage)
+    grok.require('zope.ManageSite')
+
 
 class HelpPageIndex(Form, grok.DisplayForm):
     grok.name('overview')
@@ -32,5 +44,3 @@ class HelpPageIndex(Form, grok.DisplayForm):
 class TTDisplay(grok.View):
     grok.name('index')
     grok.context(HelpPage)
-
-
