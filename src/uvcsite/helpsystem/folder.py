76ea58d12ced31dc917f745a0e6d5cb52a6d558a
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-07-20 09:12:37 +0000

    Cosmetic Changes

--- src/uvcsite/helpsystem/folder.py
+++ src/uvcsite/helpsystem/folder.py
@@ -1,14 +1,19 @@
 import grok
 import megrok.layout
 
+from uvcsite.interfaces import IUVCSite
 from uvcsite.helpsystem.interfaces import IHelpFolder
 from zope.app.authentication.interfaces import IPrincipal
+
 
 class HelpFolder(grok.Container):
     grok.implements(IHelpFolder)
 
-class HelpFolderIndex(megrok.layout.Page):
-    grok.name('index')
+
+class Index(megrok.layout.Page):
     pass
 
 
+@grok.subscribe(IUVCSite, grok.IObjectAddedEvent)
+def addHelpFolder(context, event):
+    context['hilfe'] = HelpFolder()
