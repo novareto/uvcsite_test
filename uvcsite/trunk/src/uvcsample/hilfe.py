e524afd796dbdd13e080d38856c0c556c3c1b7b1
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-07-21 13:01:30 +0000

    strip out the example

--- uvcsite/trunk/src/uvcsample/hilfe.py
+++
@@ -1,9 +0,0 @@
-import grok
-
-from uvcsample.unfall import IUnfall
-from uvcsite.helpsystem.portlet import HelpPortlet
-
-class UAZHilfe(HelpPortlet):
-    grok.context(IUnfall)
-
-    urls = [{'href': 'hilfe', 'name': 'hilfe'}]
