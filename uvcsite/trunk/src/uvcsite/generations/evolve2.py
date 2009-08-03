2b09d767134b04ec96c55027cfb58fce02d80087
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-08-03 12:25:22 +0000

    Helpsystem in a z3cform way

--- uvcsite/trunk/src/uvcsite/generations/evolve2.py
+++ uvcsite/trunk/src/uvcsite/generations/evolve2.py
@@ -1,5 +1,6 @@
 import zope.app.generations.utility
 
 def evolve(context):
-   # root = zope.app.generations.utility.getRootFolder(context)
-   # print "EVOLVE"
+    pass
+    # root = zope.app.generations.utility.getRootFolder(context)
+    # print "EVOLVE"
