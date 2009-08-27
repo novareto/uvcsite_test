31b497a25e7061eff80100553df8d6d1ed161ad6
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-08-27 08:21:42 +0000

    changes

--- uvcsite/trunk/src/uvcsite/content/tabels.py
+++ uvcsite/trunk/src/uvcsite/content/tabels.py
@@ -1,24 +1,23 @@
 import grok
 
 from uvcsite import uvcsiteMF as _
-from grok.interfaces import IContainer
 from megrok.z3ctable import (TablePage, Column, GetAttrColumn,
             CheckBoxColumn, LinkColumn, ModifiedColumn, Values)
 
 from hurry.workflow.interfaces import IWorkflowState
 from zope.dublincore.interfaces import IZopeDublinCore
 from uvcsite.workflow.basic_workflow import titleForState
-
+from uvcsite.interfaces import IFolderColumnTable
 
 class CheckBox(CheckBoxColumn):
     grok.name('checkBox')
-    grok.adapts(IContainer, None, None)
+    grok.adapts(IFolderColumnTable, None, None)
     weight = 0
 
 
 class Link(LinkColumn):
     grok.name('link')
-    grok.adapts(IContainer, None, None)
+    grok.adapts(IFolderColumnTable, None, None)
     weight = 1
     header = u"edit"
     linkName = u"edit"
@@ -27,7 +26,7 @@ class Link(LinkColumn):
 
 class MetaTypeColumn(GetAttrColumn):
     grok.name('meta_type')
-    grok.adapts(IContainer, None, None)
+    grok.adapts(IFolderColumnTable, None, None)
     header = _(u'Object')
     attrName = 'meta_type'
     weight = 2
@@ -37,7 +36,7 @@ class CreatorColumn(Column):
     grok.name('creator')
     header = u"Autor"
     weight = 99
-    grok.adapts(IContainer, None, None)
+    grok.adapts(IFolderColumnTable, None, None)
 
     def renderCell(self, item):
         return ', '.join(IZopeDublinCore(item).creators)
@@ -47,4 +46,4 @@ class ModifiedColumn(ModifiedColumn):
     grok.name('modified')
     header = u"Datum"
     weight = 100
-    grok.adapts(IContainer, None, None)
+    grok.adapts(IFolderColumnTable, None, None)
