633914f342a56e15cf44b08e9ffd342dbefa75dd
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-08-27 14:26:24 +0000

    More Graphical Changes

--- src/uvcsite/helpsystem/folder.py
+++ src/uvcsite/helpsystem/folder.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2008 NovaReto GmbH
+# cklinger@novareto.de
+
 import grok
 import megrok.z3ctable
 
@@ -7,14 +11,31 @@ from uvcsite.viewlets.utils import MenuItem
 from uvcsite.interfaces import IUVCSite, IGlobalMenu
 from uvcsite.helpsystem.interfaces import IHelpFolder
 from zope.app.authentication.interfaces import IPrincipal
+from uvcsite.skin.skin import Scripts
+from uvc.content import ProductFolder, IProductFolder, contenttype
+from page import HelpPage
+
+class HelpFolder(ProductFolder):
+    grok.implements(IProductFolder, IHelpFolder)
+    grok.title('Hilfe')
+    grok.name(u'In diesem Ordner finden Sie Hilfe Dokumente')
+    grok.description(u'Sie können auf die einzelnen Dokumente klicken um die Hilfe anzueigen!')
+
+    contenttype(HelpPage)
 
 
-class HelpFolder(grok.Container):
-    grok.implements(IHelpFolder)
+#class Index(megrok.z3ctable.TablePage):
+#    grok.require('zope.ManageSite')
+    
+#    cssClasses = {'table': 'tablesorter myTable'}
+#    cssClassEven = u'even'
+#    cssClassOdd = u'odd'
 
 
-class Index(megrok.z3ctable.TablePage):
-    grok.require('zope.ManageSite')
+
+#    def update(self):
+#        super(Index, self).update()
+#        Scripts.need()
 
 
 class Hilfe(MenuItem):
