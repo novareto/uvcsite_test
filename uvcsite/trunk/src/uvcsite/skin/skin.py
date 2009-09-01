c71f4026350050589f2f9d91621f7f81a5a0da2d
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-09-01 08:02:56 +0000

    added hurry resource instead of megrok.resorcelibrary

--- uvcsite/trunk/src/uvcsite/skin/skin.py
+++ uvcsite/trunk/src/uvcsite/skin/skin.py
@@ -4,11 +4,12 @@
 
 import grok
 import megrok.layout
-import megrok.resourcelibrary
+#import megrok.resourcelibrary
 
+from hurry.jquery import jquery
 from zope.interface import Interface
 from megrok.z3cform.skin import FormLayer, TableLayer
-
+from hurry.resource import Library, ResourceInclusion, rollup
 
 class IUVCSiteLayer(FormLayer):
     pass
@@ -18,25 +19,29 @@ class IUVCSiteSkin(IUVCSiteLayer, grok.IDefaultBrowserLayer):
     grok.skin('uvcsiteskin')
 
 
-class Styles(megrok.resourcelibrary.ResourceLibrary):
-    grok.name('uvcresources')
-    grok.layer(IUVCSiteLayer)
-    megrok.resourcelibrary.directory('styles')
-    megrok.resourcelibrary.include('main.css')
-    megrok.resourcelibrary.include('nva.css')
+class StylesDirectory(grok.DirectoryResource):
+    grok.name('styles')
+    grok.path('styles')
 
 
-class Scripts(megrok.resourcelibrary.ResourceLibrary):
-    grok.name('uvcscripts')
-    grok.layer(IUVCSiteLayer)
-    megrok.resourcelibrary.directory('javascripts')
-    megrok.resourcelibrary.include('jquery-1.3.2.js')
-    megrok.resourcelibrary.include('jtip.js')
-    megrok.resourcelibrary.include('myjs.js')
-    megrok.resourcelibrary.include('jquery.tablesorter.js')
-    megrok.resourcelibrary.include('mytable.js')
-    megrok.resourcelibrary.include('jquery-ui-1.7.2.custom.min.js')
+styles = Library('styles')
+nva = ResourceInclusion(styles, 'nva.css')
+css = ResourceInclusion(styles, 'main.css', depends=[nva])
+
+
+class ScriptsDirectory(grok.DirectoryResource):
+    grok.name('javascripts')
+    grok.path('javascripts')
+
+
+scripts = Library('javascripts')
+tablesorter = ResourceInclusion(scripts, 'jquery.tablesorter.js', [jquery])
+Scripts = ResourceInclusion(scripts, 'mytable.js', [tablesorter]) 
 
 
 class StandardLayout(megrok.layout.Layout):
     grok.context(Interface)
+
+    def update(self):
+        css.need()
+        rollup()
