790921849ae8f9661eae8d0e3ea4a249b059bb18
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-10-06 08:12:27 +0000

    Changes in viewlets und utils

--- src/uvcsite/content/ftests/folderinit.py
+++ src/uvcsite/content/ftests/folderinit.py
@@ -1,17 +1,30 @@
 """
+ProductFolder is a Utility
+==========================
 
+All our ProductFolder are at the same time utilitys
+
+  >>> from zope.app.testing.functional import getRootFolder
+  >>> app = App()
+  >>> root = getRootFolder()
+  >>> root['app'] = app
+
+  >>> import zope.app.appsetup.interfaces
+  >>> grok.notify(zope.app.appsetup.interfaces.IDatabaseOpenedWithRootEvent)
   >>> from zope.component import getUtilitiesFor
   >>> from uvcsite.content import IProductFolder
   >>> utils = list(getUtilitiesFor(IProductFolder))
+
   >>> titles = [x[0] for x in utils]
   >>> 'ENW1Container' in titles 
   True
 """
 
 import grok
+from uvcsite.app import Uvcsite
 from uvcsite.content import ProductFolder, contenttype
 
-class App(grok.Application):
+class App(Uvcsite):
     pass
 
 
@@ -24,5 +37,3 @@ class ENW1Container(ProductFolder):
     grok.title('This is a Lastschrift Container')
     grok.description('This is the Description')
     contenttype(MyContent)
-
-
