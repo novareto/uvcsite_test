7ad689f1461c28470b5445c4eb7e1ecd0995d628
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-10-06 08:27:42 +0000

    Added a Mixin Page

--- uvcsite/trunk/src/uvcsite/__init__.py
+++ uvcsite/trunk/src/uvcsite/__init__.py
@@ -2,4 +2,8 @@ from uvcsite.content import (ProductFolder, IProductFolder, contenttype,
                          IContent, Content, icon, schema, name)
 from zope.i18nmessageid import MessageFactory
 uvcsiteMF = MessageFactory('uvcsite')
+from megrok.layout import Page
 from uvcsite.content import ApplicationAwareView
+
+class Page(Page, ApplicationAwareView):
+    grok.baseclass()
