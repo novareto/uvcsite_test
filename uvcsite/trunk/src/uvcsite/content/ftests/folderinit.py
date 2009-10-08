13bd2eba8778b88ba27e1904efa25b5395618b27
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2009-10-08 13:34:01 +0000

    tests for folderinit

--- uvcsite/trunk/src/uvcsite/content/ftests/folderinit.py
+++ uvcsite/trunk/src/uvcsite/content/ftests/folderinit.py
@@ -1,24 +1,71 @@
 """
-ProductFolder is a Utility
-==========================
+Event which set's all the ProductFolders
+========================================
 
-All our ProductFolder are at the same time utilitys
+Setup
+-----
 
+  >>> import grok
   >>> from zope.app.testing.functional import getRootFolder
+  >>> from zope.app.publication.zopepublication import ZopePublication
+
+Before we fire up the DatabaseOpendEvent we have to prepare a ZODB
+
+  >>> from ZODB.tests.util import DB
+  >>> db = DB()
+  >>> conn = db.open()
+  >>> root = conn.root()
+  >>> root[ZopePublication.root_name] = grok.Container() 
+
+Let's put our app in the ZODB
+
   >>> app = App()
-  >>> root = getRootFolder()
-  >>> root['app'] = app
+  >>> from zope.app.component.hooks import getSite, setSite
+  >>> root[ZopePublication.root_name]['app'] = app
+  >>> setSite(root[ZopePublication.root_name]['app'])
+
+We need an empty HomeFolder for checking if the assigment of
+our Product folders work. This means i add the user lars to
+the HomeFolderManager
+
+  >>> from zope.component import getUtility
+  >>> from zope.app.homefolder.interfaces import IHomeFolderManager
+  >>> utility = getUtility(IHomeFolderManager)
+  >>> utility
+  <uvcsite.homefolder.homefolder.PortalMembership object at ...>
+
+We create an empty HomeFolder for lars:
+
+  >>> utility.assignHomeFolder('lars')
+  >>> 'lars' in utility.homeFolderBase
+  True
+
+Let's check that this container is empty: 
+
+  >>> lars = utility.homeFolderBase['lars']
+  >>> [x for x in lars] 
+  []
+
+  >>> import transaction
+  >>> transaction.commit()
+
+Let's call the event and look if it get called
+----------------------------------------------
 
   >>> import zope.app.appsetup.interfaces
-  >>> grok.notify(zope.app.appsetup.interfaces.IDatabaseOpenedWithRootEvent)
-  >>> from zope.component import getUtilitiesFor
-  >>> from uvcsite.content import IProductFolder
-  >>> utils = list(getUtilitiesFor(IProductFolder))
+  >>> grok.notify(zope.app.appsetup.interfaces.DatabaseOpenedWithRoot(db))
 
-  >>> titles = [x[0] for x in utils]
-  >>> 'ENW1Container' in titles 
+Ok the event is fired up. Now we should found our ENW1Container in 
+the HomeFolder of Lars.
+
+  >>> lars = utility.homeFolderBase['lars']
+  >>> 'ENW1Container' in lars 
+  True
+
+  >>> 'NotInHomeFolder' not in lars
   True
 """
+
 
 import grok
 from uvcsite.app import Uvcsite
@@ -37,3 +84,12 @@ class ENW1Container(ProductFolder):
     grok.title('This is a Lastschrift Container')
     grok.description('This is the Description')
     contenttype(MyContent)
+
+
+class NotInHomeFolder(ProductFolder):    
+    grok.name('NotInHomeFolder')
+    grok.title('This should not go into the HomeFolder')
+    grok.description('This is the Description')
+    contenttype(MyContent)
+
+    inHomeFolder = False 
