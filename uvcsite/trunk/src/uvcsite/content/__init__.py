5d095d82261863ebcd20f6c095c7e1ac42e77253
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2010-03-03 09:41:24 +0000

    remove icon

--- uvcsite/trunk/src/uvcsite/content/__init__.py
+++ uvcsite/trunk/src/uvcsite/content/__init__.py
@@ -1,4 +1,4 @@
-from dolmen.content import IContent, schema, name, icon
+from dolmen.content import IContent, schema, name
 from uvcsite.content.components import ProductFolder, Content
 from uvcsite.content.interfaces import IUVCApplication, IProductFolder, IFolderColumnTable
 from uvcsite.content.directive import contenttype 
