11c5a1dff24cd88dfa83f43926e81f748e1c61ec
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2010-05-10 07:27:59 +0000

    Tagging 0.1.11

---
+++ uvcsite/tags/0.1.11/src/uvcsite/content/ftests/views.py
@@ -0,0 +1,305 @@
+"""
+The Default Views of the ProductFolder
+======================================
+
+Setup
+-----
+
+  >>> from zope.app.testing.functional import getRootFolder
+  >>> r = getRootFolder()
+  >>> r['pf'] = pf = LastschriftContainer3()
+  >>> pf
+  <uvcsite.content.ftests.views.LastschriftContainer3 object at ...> 
+
+  >>> print pf.title
+  This is a Lastschrift Container
+
+  >>> mycontent = MyContent()
+  >>> mycontent
+  <uvcsite.content.ftests.views.MyContent ...>
+
+  >>> pf['mycontent'] = mycontent
+
+Views
+-----
+
+  >>> from zope.component import getMultiAdapter
+  >>> from zope.publisher.browser import TestRequest
+  >>> request = TestRequest()
+
+Get the ViewClass
+
+  >>> view = getMultiAdapter((pf, request), name=u"index")
+  >>> view.update() 
+  >>> view
+  <Index 'index'>
+
+Render the ViewClass
+Note: There are default columns defined for IContainer in
+      uvcsite so. No need here to define Columns
+  
+  >>> print view.render()
+  <form action="http://127.0.0.1" method="post"
+        enctype="multipart/form-data" class="edit-form"
+        name="deleteFormTable" id="deleteFormTable">
+    <div class="viewspace">
+      <div>
+      <div class="tabluarTable">
+        <table class="myTable tablesorter">
+    <thead>
+      <tr>
+        <th class="checkBox"></th>
+        <th>Titel</th>
+        <th>Objekt</th>
+        <th>Status</th>
+        <th>Autor</th>
+        <th>Datum</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="even">
+        <td><input type="checkbox" class="checkbox-widget" name="deleteFormTable-checkBox-0-selectedItems" value="mycontent"  /></td>
+        <td><a href="http://127.0.0.1/pf/mycontent/edit"></a></td>
+        <td>MyContent</td>
+        <td>Entwurf</td>
+        <td></td>
+        <td>...</td>
+      </tr>
+    </tbody>
+  </table>
+      </div>
+      <div class="tabluarForm">
+      </div>
+    </div>
+    </div>
+    <div>
+      <div class="buttons">
+  <input id="deleteFormTable-buttons-delete"
+         name="deleteFormTable.buttons.delete"
+         class="submit-widget button-field" value="Delete"
+         type="submit" />
+      </div>
+    </div>
+  </form>
+  <BLANKLINE>
+
+
+If we call this class we should see our tableform in our simple Layout:
+
+  >>> print view()
+  <html> <form action="http://127.0.0.1" method="post"
+        enctype="multipart/form-data" class="edit-form"
+        name="deleteFormTable" id="deleteFormTable">
+    <div class="viewspace">
+      <div>
+      <div class="tabluarTable">
+        <table class="myTable tablesorter">
+    <thead>
+      <tr>
+        <th class="checkBox"></th>
+        <th>Titel</th>
+        <th>Objekt</th>
+        <th>Status</th>
+        <th>Autor</th>
+        <th>Datum</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="even">
+        <td><input type="checkbox" class="checkbox-widget" name="deleteFormTable-checkBox-0-selectedItems" value="mycontent"  /></td>
+        <td><a href="http://127.0.0.1/pf/mycontent/edit"></a></td>
+        <td>MyContent</td>
+        <td>Entwurf</td>
+        <td></td>
+        <td>...</td>
+      </tr>
+    </tbody>
+  </table>
+      </div>
+      <div class="tabluarForm">
+      </div>
+    </div>
+    </div>
+    <div>
+      <div class="buttons">
+  <input id="deleteFormTable-buttons-delete"
+         name="deleteFormTable.buttons.delete"
+         class="submit-widget button-field" value="Delete"
+         type="submit" />
+      </div>
+    </div>
+  </form>
+   </html>
+
+
+Add View
+--------
+
+Let's check if our default AddForm works for our ProductFolder.
+Setting up some basic stuff for working with the testbrowser:
+
+  >>> from zope.app.testing.functional import getRootFolder
+  >>> root = getRootFolder()
+  >>> root['pf1'] = pf
+  >>> from zope.testbrowser.testing import Browser
+  >>> browser = Browser()
+  >>> browser.handleErrors = False 
+  >>> browser.addHeader('Authorization', 'Basic mgr:mgrpw')
+
+We call the add form:
+  
+  >>> browser.open('http://localhost/pf1/add')
+  >>> print browser.headers['Status'].upper()
+  200 OK
+
+We have a form in our add form:
+
+  >>> form = browser.getForm()
+  >>> form.getControl(name='form.widgets.age').value = '33'
+  >>> form.getControl(name='form.widgets.name').value = 'Klaus'
+  >>> form.getControl(name='form.widgets.title').value = 'Titel'
+  >>> form.submit("Add")
+   
+
+Edit View
+---------
+
+  >>> print browser.contents
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <html xmlns="http://www.w3.org/1999/xhtml">...
+    <tbody>
+      <tr class="even">
+        <td><input type="checkbox" class="checkbox-widget" name="deleteFormTable-checkBox-0-selectedItems" value="MyContent"  /></td>
+        <td><a href="http://localhost/pf1/MyContent/edit">Titel</a></td>
+        <td>MyContent</td>
+        <td>Entwurf</td>
+        <td>zope.mgr</td>
+        ...
+      </tr>
+      <tr class="odd">
+        <td><input type="checkbox" class="checkbox-widget" name="deleteFormTable-checkBox-0-selectedItems" value="mycontent"  /></td>
+        <td><a href="http://localhost/pf1/mycontent/edit"></a></td>
+        <td>MyContent</td>
+        <td>Entwurf</td>
+        <td></td>
+        <td>...</td>
+      </tr>
+    </tbody>
+  ...
+  </html>
+
+Get the Link for edit and:
+
+  >>> edit_link = browser.getLink('Titel')
+  >>> print edit_link.url
+  http://localhost/pf1/MyContent/edit
+
+click on it:
+
+  >>> edit_link.click()
+  >>> print browser.headers['Status'].upper()
+  200 OK
+
+Index View 
+----------
+
+  >>> browser.open('http://localhost/pf1/MyContent')
+  >>> print browser.headers['Status'].upper()
+  200 OK
+
+
+Delete Objects from a ProductFolder
+-----------------------------------
+
+Let's get again the index View of our ProductFolder. 
+We should get 'mycontent' as item in the container.
+
+  >>> browser.open('http://localhost/pf1')
+  >>> view = getMultiAdapter((pf, request), name=u"index")
+  >>> folder = view.context
+  >>> 'mycontent' in folder 
+  True
+
+The Index View of the ProductFolder has a executeDelete method
+which get's called by the deleteAction of the form. So we test
+this here manually.
+  
+  >>> browser.headers['Status'].upper()
+  '200 OK'
+
+  >>> form = browser.getForm()
+  >>> form.getControl(name='deleteFormTable-checkBox-0-selectedItems').value = ['mycontent',]
+  >>> form.submit('Delete')
+  >>> print browser.contents 
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <html xmlns="http://www.w3.org/1999/xhtml">...
+    <table class="myTable tablesorter">
+      <thead>
+        <tr>
+          <th class="checkBox"></th>
+          <th>Titel</th>
+          <th>Objekt</th>
+          <th>Status</th>
+          <th>Autor</th>
+          <th>Datum</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="even">
+          <td><input type="checkbox" class="checkbox-widget" name="deleteFormTable-checkBox-0-selectedItems" value="MyContent"  /></td>
+          <td><a href="http://localhost/pf1/MyContent/edit">Titel</a></td>
+          <td>MyContent</td>
+          <td>Entwurf</td>
+          <td>zope.mgr</td>
+          <td>...</td>
+        </tr>
+      </tbody>
+    </table>
+    ...
+    </html>
+ 
+  
+
+"""
+import grok
+
+from megrok import z3cform
+from megrok import layout
+from zope.interface import Interface
+from zope.schema import TextLine, Int
+from uvcsite.content import ProductFolder, contenttype, Content, schema
+import z3c.flashmessage.receiver
+import z3c.flashmessage.sources
+
+grok.global_utility(z3c.flashmessage.sources.SessionMessageSource,
+                    name='session')
+
+grok.global_utility(z3c.flashmessage.receiver.GlobalMessageReceiver)
+
+
+class MyLayout(layout.Layout):
+    grok.context(Interface)
+
+    def render(self):
+	return "<html> %s </html>" %(self.view.render())
+
+class IMyContent(Interface):
+    name = TextLine(title = u"Name")
+    age = Int(title = u"Age")
+
+
+class MyContent(Content):
+    grok.implements(IMyContent)    
+    grok.name('MyContent')
+    schema(IMyContent)
+
+
+class LastschriftContainer3(ProductFolder):
+    grok.name('LastschriftContainer3')
+    grok.title('This is a Lastschrift Container')
+    grok.description('This is the Description')
+    contenttype(MyContent)
+
+
---
+++ uvcsite/tags/0.1.11/src/uvcsite/tests/simpleaddon.py
@@ -0,0 +1,44 @@
+import grok
+import uvcsite
+import uvc.layout
+import zope.schema
+import zope.interface
+import zope.component
+
+class IContact(uvcsite.IContent):
+
+    name = zope.schema.TextLine(
+        title = u"Name",
+        description = u"Wie ist ihr Name",
+        )
+
+    alter = zope.schema.TextLine(
+        title = u"Alter",
+        description = u"Wie ist ihr Alter",
+        )
+
+
+class IAdressBook(uvcsite.IProductFolder):
+    """ Marker Interface """
+
+
+class Contact(uvcsite.Content):
+    grok.name(u'Kontakt')
+    uvcsite.schema(IContact)
+
+
+class AdressBook(uvcsite.ProductFolder):
+    uvcsite.contenttype(Contact)
+
+
+class AddMenuEntry(uvcsite.Entry):
+    grok.name('Buddy erstellen')
+    grok.title('Buddy erstellen')
+    grok.context(zope.interface.Interface)
+    uvcsite.menu(uvcsite.GlobalMenu)
+    uvc.layout.menus.category(u'Apps')
+
+    @property
+    def url(self):
+        adapter = zope.component.getMultiAdapter((self.request.principal, self.request), uvcsite.IGetHomeFolderUrl)
+        return adapter.getAddURL(Contact)
