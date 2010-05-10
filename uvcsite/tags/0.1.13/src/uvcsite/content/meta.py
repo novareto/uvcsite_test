3fdcff3dc4849683ee9b2e0e24ae9a46a47eb4e5
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2010-05-10 07:42:57 +0000

    Tagging 0.1.13

---
+++ uvcsite/tags/0.1.13/src/uvcsite/content/meta.py
@@ -0,0 +1,31 @@
+import martian
+import grokcore.component
+
+from martian.error import GrokError
+from uvcsite.content import ProductFolder, contenttype, IProductFolder
+import zope.component.zcml
+
+
+def default_name(factory, module=None, **data):
+    return factory.__name__.lower()
+
+
+class ProductFolderGrokker(martian.ClassGrokker):
+    martian.component(ProductFolder)
+    martian.directive(contenttype)
+    martian.directive(grokcore.component.name, get_default=default_name)
+
+    def execute(self, factory, config, contenttype, name):
+        if not contenttype:
+            raise GrokError("%r must specify which contenttype should "
+                            "go into this ProductFolder. Please use the"
+                            "direcitve 'contenttype' for it."
+                            % factory, factory)
+
+        config.action(
+            discriminator=('utility', IProductFolder, name),
+            callable=zope.component.zcml.handler,
+            args=('registerUtility', factory, IProductFolder, name),
+            )
+
+        return True
