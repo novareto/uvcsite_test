e71c83bca4a2ef3e11b4fb21c6b287d367a3592d
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2010-07-01 14:28:34 +0000

    Tagging 0.2.0

---
+++ uvcsite/tags/0.2.0/src/uvcsite/auth/forms.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2010 NovaReto GmbH
+# cklinger@novareto.de 
+
+
+import grok
+import uvcsite
+
+from zope import schema
+from zope import component
+from zope.interface import Interface
+
+from z3c.form.interfaces import HIDDEN_MODE
+from megrok.z3cform.base import PageForm, Fields, button
+
+from zope.app.security.interfaces import IAuthentication, IUnauthenticatedPrincipal, ILogout
+from zope.session.interfaces import ISession
+
+class ILoginForm(Interface):
+    login = schema.BytesLine(title=u'Mitgliedsnummer', required=True)
+    camefrom = schema.BytesLine(title=u'', required=False)
+    password = schema.Password(title=u'Passwort', required=True)
+
+
+class Login(PageForm):
+    grok.context(Interface)
+    grok.require('zope.Public')
+    label = "Herzlich Willkommen im Extranet"
+    description = u"In diesem Bereich können Sie verschiedene Application nutzten! "
+
+    prefix = ''
+    fields = Fields(ILoginForm)
+    ignoreContext = True
+
+    def updateWidgets(self):
+        super(Login, self).updateWidgets()
+        self.widgets['camefrom'].mode = HIDDEN_MODE
+
+    @button.buttonAndHandler(u'login', name="login")
+    def handle_login(self, action):
+        self.redirect(self.request.form.get('camefrom', self.url(grok.getSite())))
+
+
+class Logout(uvcsite.Page): 
+    grok.context(Interface) 
+    grok.require('zope.Public') 
+ 
+    def update(self): 
+        session = ISession(self.request)
+        session.delete()
+
+    def render(self):
+        return "Sie Sind Jetzt Ausgeloggt"
---
+++ uvcsite/tags/0.2.0/src/uvcsite/resources.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2010 NovaReto GmbH
+# cklinger@novareto.de 
+
+import grok
+
+from megrok import resource
+from hurry.jquery import jquery
+from zope.interface import Interface
+from uvc.layout.interfaces import IHeaders
+
+
+class UVCResources(resource.ResourceLibrary):
+    resource.name('uvcresource')
+    resource.path('static')
+
+    resource.resource('jquery.tablesorter.min.js')
+    resource.resource('jquery.tools.min.js')
+    resource.resource('uvc_base.js')
+    resource.resource('uvc_base.css')
+
+
+class UVCResourceViewlet(grok.Viewlet):
+    grok.viewletmanager(IHeaders)
+    grok.context(Interface)
+
+    def render(self):
+        jquery.need()
+        UVCResources.need()
+        return u''
+
