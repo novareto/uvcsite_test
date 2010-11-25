a972a6df188ebbfe363eaa22815db1b0cfc8658f
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2010-11-25 10:16:22 +0000

    better style

--- src/uvcsite/static/tooltip.js
+++ src/uvcsite/static/tooltip.js
@@ -1,26 +1,14 @@
 $(document).ready(function()
     {
-
-        $.each($('div.row'), function() {
-            var description = $(this).children('label').children('span.small').text();
-            if (description != '') { 
-                $(this).children('div.widget').children('input').attr('title', description).attr('tooltip','1');
-                $(this).children('label').children('span.small').hide();
-            }
-            else {
-                $(this).children('div.widget').children('input').attr('title', '0');
-            }
-        });
-
-
-        $("form :input[tooltip='1']").not(":button").tooltip({ 
-            position: "center right", 
-            offset: [-2, 10], 
-            effect: "fade", 
-            opacity: 0.8, 
-            tip: '.tooltip',
-        });
-
+      $('input#form-action-senden-mit-ajax').click(function(event) {
+          var data = 'KLAUS';
+          var uri = window.location;
+          $.getJSON(uri+'/@@getData', data, function(data) {
+                $('div#above-page').append(data);
+              }) 
+          event.preventDefault();
+          $('form :input').first().focus();
+      });
     }
-);
-
+)
+    
