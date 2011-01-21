167c8b2a3e81c02a64359a7b2374580006d0727c
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-01-21 14:11:10 +0000

    Makeing the Serialization more Pluggable

--- src/uvcsite/content/api.py
+++ src/uvcsite/content/api.py
@@ -3,12 +3,14 @@
 
 import grok
 
+
 from lxml import etree
+from dolmen.content import IContent
 from uvcsite.content import IProductFolder
-from z3c.schema2xml import serialize_to_tree, deserialize
 from zope.component import getMultiAdapter
-from zope.interface import Invalid
+from zope.interface import Invalid, Interface
 from zope.pagetemplate.interfaces import IPageTemplate
+from z3c.schema2xml import serialize_to_tree, deserialize
 
 
 class RestLayer(grok.IRESTLayer):
@@ -19,6 +21,7 @@ class RestLayer(grok.IRESTLayer):
 class ProductFolderRest(grok.REST):
     grok.layer(RestLayer)
     grok.context(IProductFolder)
+    grok.require('zope.View')
 
     def GET(self):
         context = self.context
@@ -36,8 +39,43 @@ class ProductFolderRest(grok.REST):
         errors = []
         content = self.context.getContentType()()
         interface = content.schema[0]
+        serializer = ISerializer(content)
+        serializer.work(self.body, interface, errors)
+
+        if not errors:
+            self.context.add(content)
+            result = etree.Element(
+                'success', 
+                name=content.meta_type,
+                id=content.__name__
+                )
+        else:
+            result = etree.Element('failure')
+            result.extend(errors)
+        return etree.tostring(result, encoding='UTF-8', pretty_print=True)
+
+
+class ISerializer(Interface):
+    """ Base Serialzer for IContent Objects
+    """
+
+    def work(payload, interface, errors)
+        """ Worker which populates self.context
+            with the contents of the payload and
+            with the help of interface
+        """
+
+
+class DefaultSerializer(grok.Adapter):
+    """ Default Serializer for IContent
+    """
+
+    grok.context(IContent)
+    grok.implements(ISerializer)
+
+    def work(self, payload, interface, errors):
         try:
-            deserialize(self.body, interface, content)
+            deserialize(payload, interface, self.context)
         except Exception, e: # Here should be a DeserializeError
             for field, (exception, element) in e.field_errors.items():
                 error = etree.Element('error', field=field.__name__,
@@ -45,15 +83,6 @@ class ProductFolderRest(grok.REST):
                 error.append(element)
                 errors.append(error)
         try:
-            interface.validateInvariants(content)
+            interface.validateInvariants(self.context)
         except Invalid, e:
             errors.append(etree.Element('error', text="Invariant: %s" % e))
-
-        if not errors:
-            id = "%s-%s" %(content.meta_type, str(len(self.context)))
-            self.context[id] = content
-            result = etree.Element('success', name=content.meta_type, id=id)
-        else:
-            result = etree.Element('failure')
-            result.extend(errors)
-        return etree.tostring(result, encoding='UTF-8', pretty_print=True)
