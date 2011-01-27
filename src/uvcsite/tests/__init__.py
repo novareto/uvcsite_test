1685b646b43d05607581a8caff07ff23b169becf
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-01-27 10:35:45 +0000

    changes

--- src/uvcsite/tests/__init__.py
+++ src/uvcsite/tests/__init__.py
@@ -24,31 +24,3 @@ ftesting_zcml = os.path.join(
     os.path.dirname(uvcsite.__file__), 'ftesting_uvc.zcml')
 FunctionalLayer = ZCMLLayer(ftesting_zcml, __name__, 'FunctionalLayer',
                             allow_teardown=True, product_config=product_config)
-
-
-def setUp(test):
-    FunctionalTestSetup().setUp()
-    if getattr(sys.modules[test.name], '__grok__', True):
-        grok.testing.grok(test.name)
-
-
-def tearDown(test):
-    FunctionalTestSetup().tearDown()
-
-
-def suiteFromPackage(name):
-    files = resource_listdir(name.split('.')[0], '/'.join(name.split('.')[1:]))
-    suite = unittest.TestSuite()
-    for filename in files:
-        if not filename.endswith('.py'):
-            continue
-        if filename == '__init__.py':
-            continue
-
-        dottedname = '%s.%s' % (name, filename[:-3])
-        test = doctest.DocTestSuite(
-            dottedname, setUp=setUp, tearDown=tearDown,
-            optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
-        test.layer = FunctionalLayer
-        suite.addTest(test)
-    return suite
--- src/uvcsite/tests/test_auto.py
+++
@@ -1,6 +0,0 @@
-import z3c.testsetup
-import uvcsite
-
-test_suite = z3c.testsetup.register_all_tests('uvcsite',
-    layer=uvcsite.tests.FunctionalLayer,
-    allow_teardown=False)
