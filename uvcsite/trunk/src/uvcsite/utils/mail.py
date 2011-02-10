72620dccdd379057be823d6e48b5013d91270ccf
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-02-10 13:41:12 +0000

    added a sendmail

--- uvcsite/trunk/src/uvcsite/utils/mail.py
+++ uvcsite/trunk/src/uvcsite/utils/mail.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2010 NovaReto GmbH
+# cklinger@novareto.de 
+
 import grok
 import zope.app.appsetup.product
 import zope.component
@@ -5,6 +9,15 @@ import zope.sendmail.delivery
 import zope.sendmail.interfaces
 import zope.sendmail.mailer
 import zope.sendmail.queue
+import email.MIMEText
+import email.Header
+
+
+from email.MIMEBase import MIMEBase
+from email import Encoders
+from email.mime.multipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.Utils import COMMASPACE
 
 
 config = zope.app.appsetup.product.getProductConfiguration('mailer')
@@ -17,7 +30,7 @@ password = config.get('password', None) or None
 
 
 mailer_object = zope.sendmail.mailer.SMTPMailer(
-        hostname, port, username, password)
+        hostname, port, username, password, force_tls=True)
 
 
 def mailer():
@@ -33,6 +46,38 @@ def start_processor_thread():
     thread.setMailer(mailer_object)
     thread.setQueuePath(queue_path)
     thread.start()
+
+
+def send_mail(sender, recipient, subject, body, file=None):
+    '''
+    Funktion zum versenden von Emails
+    recipient muss als Liste übergeben werden
+    body sollte einen formatierter String sein
+    '''
+    filename = file.split("/")
+    filename = filename[-1]
+   
+    msg = MIMEMultipart()
+    msg["From"] = sender
+    msg["To"] = COMMASPACE.join(recipient)  # List to String
+    msg["Subject"] = email.Header.Header(subject, 'UTF-8')
+    msg.attach(MIMEText(body.encode('utf-8'),'plain','utf-8'))
+   
+    # Attachment von Dateien
+    if file!=None:
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(open(file, 'rb').read())
+        Encoders.encode_base64(part)
+        part.add_header(
+            'Content-Disposition', 
+            'attachment; filename=%s' % filename
+            )
+        msg.attach(part)
+    mailer = zope.component.getUtility(
+        zope.sendmail.interfaces.IMailDelivery, 
+        name=u'uvcsite.maildelivery'
+        )
+    mailer.send(sender, recipient, msg.as_string())
 
 
 grok.global_utility(
