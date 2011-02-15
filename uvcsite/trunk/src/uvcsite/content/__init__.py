5bbf1ead0aa0278547ad65281d5bb5a6a746aa0b
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-02-15 12:50:14 +0000

    translating title

--- uvcsite/trunk/src/uvcsite/content/__init__.py
+++ uvcsite/trunk/src/uvcsite/content/__init__.py
@@ -1,4 +1,13 @@
+from zope.schema import TextLine
+from uvcsite.content.directive import contenttype 
 from dolmen.content import schema, name, IContent
 from uvcsite.content.components import ProductFolder, Content
 from uvcsite.content.interfaces import IUVCApplication, IProductFolder, IFolderColumnTable
-from uvcsite.content.directive import contenttype 
+
+
+class IContent(IContent):
+
+    title = TextLine(
+        title = u"Titel",
+        description = u"Bitte geben Sie einen Titel an.",
+        required = True)
