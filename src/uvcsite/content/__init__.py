5c4feb2a218466397d3b571c2a1eef5eaf9caff8
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-02-16 14:14:54 +0000

    bugs

--- src/uvcsite/content/__init__.py
+++ src/uvcsite/content/__init__.py
@@ -1,13 +1,5 @@
 from zope.schema import TextLine
+from dolmen.content import schema, name
 from uvcsite.content.directive import contenttype 
-from dolmen.content import schema, name, IContent
 from uvcsite.content.components import ProductFolder, Content
-from uvcsite.content.interfaces import IUVCApplication, IProductFolder, IFolderColumnTable
-
-
-class IContent(IContent):
-
-    title = TextLine(
-        title = u"Titel",
-        description = u"Bitte geben Sie einen Titel an.",
-        required = True)
+from uvcsite.content.interfaces import IContent, IUVCApplication, IProductFolder, IFolderColumnTable
