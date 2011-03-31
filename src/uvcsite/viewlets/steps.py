bfb58fca42004943649eec48782b64c6bf1065be
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-03-31 11:42:21 +0000

    new wizard step menu

---
+++ src/uvcsite/viewlets/steps.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2010 NovaReto GmbH
+# cklinger@novareto.de 
+
+import grok
+import uvcsite
+from zope.interface import Interface
+
+
+grok.templatedir('templates')
+
+
+class StepsProgressBar(grok.Viewlet):
+    grok.context(Interface)
+    grok.view(uvcsite.Wizard)
+    grok.viewletmanager(uvcsite.IAboveContent)
+    grok.order(2000)
+
+
+    def update(self):
+        self.steps = []
+        current = self.view.step
+        self.title = "Schritt: %s von %s" %(current+1, len(self.view.subforms))
+        for i, step in enumerate(self.view.subforms):
+            if i == current:
+                css = "current"
+                self.title = "%s - %s  +" %(self.title, step.label)
+            elif i > current:
+                css = "future"
+            elif i < current:
+                css = "past"
+            self.steps.append(
+                dict(description = step.label, css=css,)
+                )
+        print self.steps
