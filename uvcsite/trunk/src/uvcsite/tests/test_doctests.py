a79802eec7dbf434ad29121fbde14b1824b312fb
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-10-24 09:34:26 +0000

    starting with dataviews and tests

--- uvcsite/trunk/src/uvcsite/tests/test_doctests.py
+++ uvcsite/trunk/src/uvcsite/tests/test_doctests.py
@@ -24,7 +24,8 @@ def test_suite():
         'workflow/workflow.txt', 'workflow/basic_workflow.txt',
         'content/ftests/api.py', 'content/ftests/homefoldertest.py', 
         'utils/beaker.txt', 'utils/roles.txt', 'utils/tales.txt',
-        'stat/stat.txt', 'adhoc/adhoc.txt', 'auth/group.txt',
+        'stat/stat.txt', 'adhoc/adhoc.txt', 'auth/group.txt', 'utils/pdf.txt',
+        'utils/xml.txt',
         package="uvcsite", 
         globs={'__name__': 'uvcsite'},
         optionflags=doctest.ELLIPSIS|
