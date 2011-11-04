f9bdbd7c2c97597dcf833a009fcd2281296cc6c6
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-11-04 14:54:55 +0000

    better statistic

--- src/uvcsite/stat/views.py
+++ src/uvcsite/stat/views.py
@@ -9,6 +9,7 @@ from hurry.workflow.interfaces import IWorkflowState
 
 from zope.component import getUtility
 from zope.app.homefolder.interfaces import IHomeFolderManager
+from ZODB.interfaces import IBroken
 
 
 grok.templatedir('templates')
@@ -24,17 +25,20 @@ class StatistikView(uvcsite.Page):
 
     def update(self):
         self.counter = dict()
-        inc = dict(anzahl=0, entwurf=0, gesendet=0)
+        inc = dict(anzahl=0, entwurf=0, gesendet=0, verarbeitung=0)
         hFB = getUtility(IHomeFolderManager).homeFolderBase
         self.counter['HomeFolder'] = inc
         self.counter['HomeFolder']['anzahl'] = len(hFB)
         for homefolder in hFB.values():
             for name, pf in homefolder.items():
-                if name not in self.counter:
-                    self.counter[name] = dict(anzahl=0, entwurf=0, gesendet=0) 
-                self.counter[name]['anzahl'] += len(pf)
-                for obj in pf.values():
-                    if IWorkflowState(obj).getState() == 0:
-                        self.counter[name]['entwurf'] += 1 
-                    elif IWorkflowState(obj).getState() == 1:
-                        self.counter[name]['gesendet'] += 1 
+                if not IBroken.providedBy(pf):
+                    if name not in self.counter:
+                        self.counter[name] = dict(anzahl=0, entwurf=0, gesendet=0, verarbeitung=0) 
+                    self.counter[name]['anzahl'] += len(pf)
+                    for obj in pf.values():
+                        if IWorkflowState(obj).getState() == 0:
+                            self.counter[name]['entwurf'] += 1 
+                        elif IWorkflowState(obj).getState() == 1:
+                            self.counter[name]['gesendet'] += 1 
+                        elif IWorkflowState(obj).getState() == 2:
+                            self.counter[name]['verarbeitung'] += 1 
