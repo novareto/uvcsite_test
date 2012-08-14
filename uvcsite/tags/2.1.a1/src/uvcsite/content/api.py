e5bb8c845aa2464a65c26b0e008dc11fc94d2723
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2012-08-14 09:33:34 +0000

    Tagging 2.1.a1

---
+++ uvcsite/tags/2.1.a1/src/uvcsite/content/api.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import grok
+import uvcsite
+
+from lxml import etree
+from lxml.builder import E
+from dolmen.content import IContent
+from uvcsite.content import IProductFolder
+from zope.component import getMultiAdapter
+from zope.interface import Invalid, Interface
+from hurry.workflow.interfaces import IWorkflowState
+from zope.pagetemplate.interfaces import IPageTemplate
+from z3c.schema2xml import serialize_to_tree, deserialize
+from uvcsite.workflow.basic_workflow import titleForState
+from uvc.layout.forms.event import AfterSaveEvent
+
+
+class RestLayer(grok.IRESTLayer):
+    """ Layer for Rest Access"""
+    grok.restskin('api')
+
+
+class ProductFolderRest(grok.REST):
+    grok.layer(RestLayer)
+    grok.context(IProductFolder)
+    grok.require('zope.View')
+
+    def GET(self):
+        context = self.context
+        container = E('container', id=context.__name__)
+        for id, obj in self.context.items():
+            state = titleForState(IWorkflowState(obj).getState())
+            container.append(
+                E(obj.meta_type,
+                    E('id', obj.__name__),
+                    E('titel', obj.title),
+                    E('author', obj.principal.id),
+                    E('datum', obj.modtime.strftime('%d.%m.%Y')),
+                    E('status', state)
+                ))
+        return etree.tostring(container, xml_declaration=True, encoding='utf-8', pretty_print=True)
+
+    def PUT(self):
+        errors = []
+        content = self.context.getContentType()()
+        interface = content.schema[0]
+        serializer = ISerializer(content)
+        serializer.work(self.body, interface, errors)
+
+        if not errors:
+            self.context.add(content)
+            result = etree.Element(
+                'success', 
+                name=content.meta_type,
+                id=content.__name__
+                )
+            grok.notify(AfterSaveEvent(content, self.request))
+        else:
+            result = etree.Element('failure')
+            result.extend(errors)
+        return etree.tostring(result, encoding='UTF-8', pretty_print=True)
+
+
+class ContentRest(grok.REST):
+    grok.layer(RestLayer)
+    grok.context(uvcsite.IContent)
+    grok.require('zope.View')
+
+    def GET(self):
+        context = self.context
+        id = context.__name__
+        object = etree.Element('unfallanzeige', id=id)
+        schema = context.schema[0]
+        element = etree.SubElement(object, context.meta_type, id=id)
+        serialize_to_tree(element, schema, context)
+        return etree.tostring(object, xml_declaration=True, encoding='UTF-8', pretty_print=True)
+ 
+
+
+class ISerializer(Interface):
+    """ Base Serialzer for IContent Objects
+    """
+
+    def work(payload, interface, errors):
+        """ Worker which populates self.context
+            with the contents of the payload and
+            with the help of interface
+        """
+
+
+class DefaultSerializer(grok.Adapter):
+    """ Default Serializer for IContent
+    """
+
+    grok.context(IContent)
+    grok.implements(ISerializer)
+
+    def work(self, payload, interface, errors):
+        try:
+            deserialize(payload, interface, self.context)
+        except Exception, e: # Here should be a DeserializeError
+            for field, (exception, element) in e.field_errors.items():
+                error = etree.Element('error', field=field.__name__,
+                    message=exception.__doc__)
+                error.append(element)
+                errors.append(error)
+        try:
+            interface.validateInvariants(self.context)
+        except Invalid, e:
+            errors.append(etree.Element('error', text="Invariant: %s" % e))
---
+++ uvcsite/tags/2.1.a1/src/uvcsite/stat/views.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2011 NovaReto GmbH
+# cklinger@novareto.de 
+
+import grok
+import uvcsite
+
+from hurry.workflow.interfaces import IWorkflowState
+
+from zope.interface import Interface
+from zope.component import getUtility
+from zope.app.homefolder.interfaces import IHomeFolderManager
+from ZODB.interfaces import IBroken
+
+
+grok.templatedir('templates')
+
+
+class StatistikMenu(uvcsite.MenuItem):
+    grok.context(uvcsite.IUVCSite)
+    grok.title('Statistik')
+    grok.require('zope.ManageSite')
+    grok.viewletmanager(uvcsite.IFooter)
+
+    @property
+    def action(self):
+        return "%s/statistik" % self.view.application_url()
+
+
+class StatistikView(uvcsite.Page):
+    grok.name('statistik')
+    grok.title('Statistik')
+    grok.require('zope.ManageSite')
+
+    grok.context(uvcsite.IUVCSite)
+
+    def update(self):
+        self.counter = dict()
+        inc = dict(anzahl=0, entwurf=0, gesendet=0, verarbeitung=0)
+        hFB = getUtility(IHomeFolderManager).homeFolderBase
+        self.counter['HomeFolder'] = inc
+        self.counter['HomeFolder']['anzahl'] = len(hFB)
+        pf_c = 0
+        for homefolder in hFB.values():
+            for name, pf in homefolder.items():
+                if not IBroken.providedBy(pf):
+                    pf_c += 1
+                    if name not in self.counter:
+                        self.counter[name] = dict(anzahl=0, entwurf=0, gesendet=0, verarbeitung=0) 
+                    self.counter[name]['anzahl'] += len(pf)
+                    for obj in pf.values():
+                        state = IWorkflowState(obj).getState()
+                        if state == 0:
+                            self.counter[name]['entwurf'] += 1 
+                        elif state == 1:
+                            self.counter[name]['gesendet'] += 1 
+                        elif state == 2:
+                            self.counter[name]['verarbeitung'] += 1 
+        self.counter['ProductFolders'] = dict() 
+        self.counter['ProductFolders']['anzahl'] = pf_c
