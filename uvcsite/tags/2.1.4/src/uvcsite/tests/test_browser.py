502f75ad9622e2089e232f7f0fd12475d41c66e6
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2012-10-15 09:21:01 +0000

    Tagging 2.1.4

---
+++ uvcsite/tags/2.1.4/src/uvcsite/tests/test_browser.py
@@ -0,0 +1,54 @@
+import unittest
+import transaction
+import uvcsite.tests
+import datetime
+
+from megrok.layout import ILayout
+from mock import Mock
+from uvcsite.app import Uvcsite, CustomDateFieldWidget
+from zeam.form.base import Form
+from zope import component
+from zope import interface, component
+from zope.authentication.interfaces import IAuthentication
+from zope.pluggableauth.authentication import PluggableAuthentication
+from zope.publisher.browser import TestRequest
+from zope.publisher.browser import applySkin
+from zope.testbrowser.wsgi import Browser
+
+
+class ApplicationTests(uvcsite.tests.TestCase):
+
+    layer = uvcsite.tests.UVCBrowserLayer
+
+    def test_known_application(self):
+        root = self.getRootFolder()
+        app = root['app']
+        self.assertIsInstance(app, Uvcsite)
+
+
+    def test_authentication_infrastucture(self):
+        auth_util = component.getUtility(IAuthentication)
+        self.assertIsInstance(auth_util, PluggableAuthentication)
+
+    def test_bla(self):
+        browser = Browser()
+
+
+class ZeamFormOverridesTests(unittest.TestCase):
+
+    def test_zeam_widget_extraction(self):
+        pass
+
+    def test_custom_date_field_widget(self):
+        obj = Mock()
+        obj.prefix = "objprefix"
+        obj.identifier= "objident"
+        obj.htmlAttributes = {}
+        obj.valueType = "medium"
+        request = TestRequest()
+        form = Form(obj, request)
+        form.prefix="form.prefix"
+        datefield = CustomDateFieldWidget(obj, form, request)
+        datum = datetime.date(2011,01,01)
+        self.assertEqual(datefield.valueToUnicode(datum), u'2011 1 1 ')
+        
---
+++ uvcsite/tags/2.1.4/src/uvcsite/utils/shorties.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2011 NovaReto GmbH
+# cklinger@novareto.de
+
+
+import pytz
+import uvcsite
+import zope.security
+
+from zope.app.homefolder.interfaces import IHomeFolder
+from zope.app.security.interfaces import IUnauthenticatedPrincipal
+
+
+def getHomeFolder(request):
+    principal = request.principal
+    if IUnauthenticatedPrincipal.providedBy(principal):
+        return
+    return IHomeFolder(principal).homeFolder
+
+
+def getHomeFolderUrl(request, suffix=""):
+    return uvcsite.IGetHomeFolderUrl(request).getURL(type=suffix)
+
+
+def fmtDateTime(object, fmt="%d.%m.%Y %H:%M:%S"):
+    tz = pytz.timezone("Europe/Berlin")
+    return object.astimezone(tz).strftime(fmt)
+
+
+def getRequest():
+    return zope.security.management.getInteraction().participations[0]
+
+
+def getPrincipal():
+    return getRequest().principal
