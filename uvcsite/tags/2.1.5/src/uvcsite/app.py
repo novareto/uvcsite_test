e8fc39ab3aa271b6db29d252293d1a95f8cc4007
ck@novareto.de <ck@novareto.de@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2013-02-01 08:54:37 +0000

    Tagging 2.1.5

---
+++ uvcsite/tags/2.1.5/src/uvcsite/app.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+import grok
+import uvcsite
+import zope.component
+
+
+from dolmen.app.site import IDolmen
+
+from uvcsite.auth.handler import UVCAuthenticator
+from uvcsite.homefolder.homefolder import PortalMembership
+
+from zope.interface import Interface
+from zope.component.interfaces import IComponents
+from zope.pluggableauth import PluggableAuthentication
+from grokcore.registries import create_components_registry
+from zope.authentication.interfaces import IAuthentication
+from zope.app.homefolder.interfaces import IHomeFolderManager
+from zope.pluggableauth.interfaces import IAuthenticatorPlugin
+
+from zope.i18n.interfaces import IUserPreferredLanguages
+from zope.publisher.interfaces.http import IHTTPRequest
+from zeam.form.ztk.widgets.choice import RadioFieldWidget
+from zeam.form.ztk.widgets.collection import MultiChoiceFieldWidget
+from zeam.form.ztk import customize
+from zope.schema.interfaces import IDate
+
+
+grok.templatedir('templates')
+
+
+def setup_pau(PAU):
+    PAU.authenticatorPlugins = ('principals', )
+    PAU.credentialsPlugins = ("cookies",
+                              "Zope Realm Basic-Auth",
+                              "No Challenge if Authenticated",)
+
+
+class Icons(grok.DirectoryResource):
+    """Directory Resource for Icons like pdf.png
+    """
+    grok.name('uvc-icons')
+    grok.path('icons')
+
+
+uvcsiteRegistry = create_components_registry(
+    name="uvcsiteRegistry",
+    bases=(zope.component.globalSiteManager, ),
+)
+
+
+grok.global_utility(uvcsiteRegistry, name="uvcsiteRegistry", provides=IComponents, direct=True)
+
+
+class Uvcsite(grok.Application, grok.Container):
+    """Application Object for uvc.site
+    """
+    grok.implements(uvcsite.IUVCSite, IDolmen)
+
+    grok.local_utility(PortalMembership,
+                       provides=IHomeFolderManager)
+
+    grok.local_utility(UVCAuthenticator,
+                       name=u"principals",
+                       provides=IAuthenticatorPlugin)
+
+    grok.local_utility(PluggableAuthentication,
+                       IAuthentication,
+                       public=True,
+                       setup=setup_pau)
+
+    def getSiteManager(self):
+        current = super(Uvcsite, self).getSiteManager()
+        if uvcsiteRegistry not in current.__bases__:
+            uvcsiteRegistry.__bases__ = tuple([x for x in uvcsiteRegistry.__bases__ if x.__hash__() != zope.component.globalSiteManager.__hash__()])
+            current.__bases__ += (uvcsiteRegistry,)
+        return current
+
+
+class NotFound(uvcsite.Page, grok.components.NotFoundView):
+    """Not Found Error View
+    """
+    pass
+
+
+class SystemError(uvcsite.Page, grok.components.ExceptionView):
+    """Custom System Error for UVCSITE
+    """
+
+    def __init__(self, context, request):
+        super(SystemError, self).__init__(context, request)
+        self.context = grok.getSite()
+        self.origin_context = context
+
+
+@customize(origin=IDate)
+def customize_size(field):
+    field.valueLength = 'medium'
+
+
+class Favicon(grok.View):
+    """ Helper for Favicon.ico Errors Request
+    """
+    grok.context(Interface)
+    grok.name('favicon.ico')
+    grok.require('zope.Public')
+
+    def render(self):
+        return "BLA"
+
+
+class GermanBrowserLangugage(grok.Adapter):
+    grok.context(IHTTPRequest)
+    grok.implements(IUserPreferredLanguages)
+
+    def getPreferredLanguages(self):
+        return ['de', 'de-de']
+
+
+class UvcRadioFieldWidget(RadioFieldWidget):
+    """ Simple Override for removing <br> between choices
+    """
+    pass
+
+
+class UvcMultiChoiceFieldWidget(MultiChoiceFieldWidget):
+    """ Simple Override for removing <br> between choices
+    """
+
+
+class HAProxyCheck(grok.View):
+    grok.context(uvcsite.IUVCSite)
+    grok.require('zope.Public')
+
+    def render(self):
+        return "OK"
