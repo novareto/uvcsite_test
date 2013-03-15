f5551e8b96a709a39830114ebcfacc32527433fa
ck@novareto.de <ck@novareto.de@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2013-03-15 08:52:17 +0000

    more shortcuts ideen von klaus

--- uvcsite/trunk/src/uvcsite/utils/shorties.py
+++ uvcsite/trunk/src/uvcsite/utils/shorties.py
@@ -8,6 +8,7 @@ import urllib
 import uvcsite
 import zope.security
 
+from datetime import datetime, date
 from zope.app.homefolder.interfaces import IHomeFolder
 from zope.app.security.interfaces import IUnauthenticatedPrincipal
 
@@ -31,6 +32,24 @@ def fmtDateTime(object, fmt="%d.%m.%Y %H:%M:%S"):
     return object.astimezone(tz).strftime(fmt)
 
 
+def fmtDate(p_date):
+    if p_date is None:
+        return ''
+    tz = pytz.timezone('Europe/Berlin')
+    if isinstance(p_date, datetime):
+        if p_date.tzinfo:
+            tz = p_date.tzinfo
+        p_date = datetime(p_date.year, p_date.month, p_date.day, tzinfo=tz)
+        return fmtDateTime(p_date, fmt='%d.%m.%Y')
+    if isinstance(p_date, date):
+        p_date = datetime(p_date.year, p_date.month, p_date.day, tzinfo=tz)
+    if isinstance(p_date, int):
+        p_date = str(p_date)
+    if isinstance(p_date, str):
+        p_date = datetime(int(p_date[0:4]), int(p_date[4:6]), int(p_date[6:8]), tzinfo=tz)
+    return fmtDateTime(p_date, fmt='%d.%m.%Y')
+
+
 def fmtZahl(n):
     if isinstance(n, str) or isinstance(n, int):
         r = []
@@ -40,6 +59,14 @@ def fmtZahl(n):
             r.insert(0, c)
         return ''.join(r)
     return n
+
+
+def fmtFloat(f, dp=2):
+    if f is None:
+        return ''
+    assert(isinstance(f, float))
+    vk, nk = str('%.*f' % (dp, f)).split('.')
+    return "%s,%s" % (fmtZahl(vk), str(nk))
 
 
 def getRequest():
