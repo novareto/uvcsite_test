62b032060f8631350ccd1cbde7e140d955db829f
ck@novareto.de <ck@novareto.de@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2013-03-21 12:42:11 +0000

    Tagging 2.1.6.1

---
+++ uvcsite/tags/2.1.6.1/src/uvcsite/tests/simpleaddon.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2011 NovaReto GmbH
+# cklinger@novareto.de
+
+import grok
+import uvcsite
+import transaction
+import zope.schema
+import zope.interface
+import zope.component
+import zope.security
+
+from StringIO import StringIO
+from elementtree.SimpleXMLWriter import XMLWriter
+from hurry.workflow.interfaces import IWorkflowInfo
+from uvc.validation import validation
+from uvcsite.content.directive import productfolder
+from uvcsite.content.productregistration import ProductMenuItem
+from uvcsite.content.productregistration import ProductRegistration
+from xml.dom.minidom import parseString
+
+
+desc_name = u"""Wie ist Ihr <a rel="tooltip" href="#" data-original-title="Default tooltip">Name</a>"""
+
+
+class IContact(uvcsite.IContent):
+
+    name = zope.schema.TextLine(
+        title=u"Name",
+        description=desc_name,
+    )
+
+    alter = zope.schema.TextLine(
+        title=u"Alter",
+        description=u"Wie ist ihr Alter",
+        required=False,
+        constraint=validation.validateZahl
+    )
+
+
+class IAdressBook(uvcsite.IProductFolder):
+    """ Marker Interface """
+
+
+class Contact(uvcsite.Content):
+    grok.implements(IContact)
+    grok.name(u'Kontakt')
+    uvcsite.schema(IContact)
+
+
+class AdressBook(uvcsite.ProductFolder):
+    grok.implements(IAdressBook)
+    grok.name('adressbook')
+    grok.title('Adressbuch')
+    grok.description('Adressbuch ...')
+    uvcsite.contenttype(Contact)
+
+    @property
+    def excludeFromNav(self):
+        principal = zope.security.management.getInteraction().participations[0].principal
+        if principal.id == "0202020002":
+            return True
+        return False
+
+
+class StatMenu(uvcsite.MenuItem):
+    grok.context(AdressBook)
+    grok.viewletmanager(uvcsite.IExtraViews)
+    grok.title('Statistik')
+
+    action = "stat"
+
+
+class ADMenu(grok.Viewlet):
+    grok.viewletmanager(uvcsite.IAboveContent)
+    grok.context(AdressBook)
+    grok.order(40)
+
+    def render(self):
+        url = self.view.url(self.context, 'stat')
+        return "<ul class='nav'> <li class='dropdown'> <a class='dropdown-toggle' href=%s> Alte Dokumente </a> </li> </ul>" % url
+
+
+class Stat(uvcsite.Page):
+    grok.name('stat')
+    grok.title('Statistik LONG LONG LONG')
+    grok.context(AdressBook)
+    viewName = "stat"
+    title = "title"
+
+    def render(self):
+        return "<div> <h1>Statistiks</h1> </div>"
+
+
+@grok.subscribe(Contact, uvcsite.IAfterSaveEvent)
+def handle_save(obj, event):
+    sp = transaction.savepoint()
+    try:
+        pdf = zope.component.getMultiAdapter((obj, event.request), name=u"pdf")
+        pdf.create(fn="/tmp/kk/%s.pdf" % (obj.__name__))
+        1 / 0
+        IWorkflowInfo(obj).fireTransition('publish')
+    except StandardError, e:
+        sp.rollback()
+        IWorkflowInfo(obj).fireTransition('progress')
+        uvcsite.logger.exception("FUCK OFF")
+        uvcsite.log('simpleaddon', e.__doc__)
+    print "AfterSaveEvent"
+
+
+class AddMenuEntry(ProductMenuItem):
+    grok.name('Buddy erstellen')
+    grok.title('Buddy erstellen')
+    grok.context(zope.interface.Interface)
+    grok.viewletmanager(uvcsite.IGlobalMenu)
+
+    @property
+    def reg_name(self):
+        return "adressbook"
+
+
+class Addressbook(ProductRegistration):
+    grok.name('adressbook')
+    grok.title('Adressbuch')
+    grok.description('Beschreibung Entgeltnachweis')
+    productfolder('uvcsite.tests.simpleaddon.AdressBook')
+
+
+def kopf(c):
+    c.drawString(200, 200, u"Ich bin der KOPF")
+
+
+class KontaktPdf(uvcsite.BasePDF):
+    grok.context(IContact)
+    grok.name('pdf')
+
+    def generate(self):
+        c = self.c
+        kopf(c)
+        c.drawString(100, 100, "Hello World")
+        c.drawString(300, 300, self.request.principal.id)
+        c.drawString(400, 400, self.context.name)
+        c.showPage()
+
+
+class KontaktXML(uvcsite.BaseXML):
+    grok.context(IContact)
+    grok.name('xml')
+    grok.title('kontakt.xml')
+
+    def generate(self):
+        io = StringIO()
+        w = XMLWriter(io, encoding="utf-8")
+        kon = w.start('kontakt')
+        w.start('basis')
+        w.element('creator', self.request.principal.id)
+        w.element('name', self.context.name)
+        w.end()
+        w.close(kon)
+        io.seek(0)
+        self.xml_file.write(parseString(io.read()).toprettyxml(indent="  "))
---
+++ uvcsite/tags/2.1.6.1/src/uvcsite/utils/dataviews.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2011 NovaReto GmbH
+# cklinger@novareto.de
+
+import grok
+import tempfile
+
+from reportlab.pdfgen import canvas
+from repoze.filesafe import create_file
+from zope.interface import Interface
+
+
+class BaseDataView(grok.View):
+    grok.context(Interface)
+    grok.baseclass()
+    grok.name('basedateview')
+    grok.title('basedataview')
+    content_type = ""
+
+    def getFile(self, fn):
+        if fn:
+            return create_file(fn, 'w+b')
+        return tempfile.TemporaryFile()
+
+    @property
+    def filename(self):
+        return grok.title.bind().get(self)
+
+    def update(self, filename=None):
+        self.base_file = self.getFile(filename)
+        self.generate()
+
+    def create(self, fn):
+        """schreibt die Datei weg und schliesst das File"""
+        self.update(fn)
+        self.base_file.close()
+
+    def genpdf(self):
+        return self.generate()
+
+    def generate(self):
+        """Methode muss von jedem Konsumenten ausprogrammiert werden,
+           weil diese Methode beim Aufruf von der Methode update aufgerufen wird."""
+        raise NotImplementedError
+
+    def render(self):
+        currentfile = self.base_file
+        currentfile.seek(0)
+        RESPONSE = self.request.response
+        RESPONSE.setHeader('content-type', self.content_type)
+        RESPONSE.setHeader('content-length', currentfile)
+        RESPONSE.setHeader('content-disposition', 'attachment; filename=%s' %self.filename)
+        return currentfile
+
+
+class BasePDF(BaseDataView):
+    grok.baseclass()
+    grok.name('pdf')
+    grok.title('uvcsite.pdf')
+    content_type = "application/pdf"
+
+    def update(self, filename=None):
+        self.base_file = self.getFile(filename)
+        self.c = canvas.Canvas(self.base_file)
+        self.genpdf()
+        self.c.save()
+
+
+class BaseXML(BaseDataView):
+    grok.name('xml')
+    grok.title('uvcsite.xml')
+    grok.baseclass()
+    content_type = "application/xml"
