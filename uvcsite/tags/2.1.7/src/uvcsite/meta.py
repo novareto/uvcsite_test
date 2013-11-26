d14d9df6fd00b2a1b9b45f64d511838ff36cc971
ck@novareto.de <ck@novareto.de@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2013-11-26 14:46:38 +0000

    first shot

---
+++ uvcsite/tags/2.1.7/src/uvcsite/meta.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import martian
+from uvcsite.app import Uvcsite
+from grokcore.site.interfaces import IApplication
+from grokcore.component import provideUtility
+
+
+class UVCSiteGrokker(martian.ClassGrokker):
+    """Grokker for Grok application classes."""
+    martian.component(Uvcsite)
+    martian.priority(500)
+
+    def grok(self, name, factory, module_info, config, **kw):
+        import pdb; pdb.set_trace()
+        provides = IApplication
+        name = '%s.%s' % (module_info.dotted_name, name)
+        config.action(
+            discriminator=('utility', provides, name),
+            callable=provideUtility,
+            args=(factory, provides, name),
+            )
+        return True
+
