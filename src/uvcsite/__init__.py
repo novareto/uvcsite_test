7b96685150ecfe834067ced66204008fc0c1078c
trollfot <trollfot@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2014-06-17 16:15:16 +0000

    We're now importing from uvc.api prior to other sub-packages. Still a lot to be done here

--- src/uvcsite/__init__.py
+++ src/uvcsite/__init__.py
@@ -12,14 +12,11 @@ from uvcsite.utils.help import HelpPage
 from uvc.layout.slots.menus import  (Footer, GlobalMenu,
     PersonalMenu, PersonalPreferences, DocumentActionsMenu)
 from uvcsite.utils.dataviews import BasePDF, BaseXML, BaseDataView, WatermarkPDF
-from uvc.layout.forms import Form, AddForm, SubForm, GroupForm, Wizard, Step
-from uvc.layout import *
-
-#from uvc.layout.event import IAfterSaveEvent
+from uvc.layout.forms import AddForm, SubForm, GroupForm, Wizard, Step
+from uvc.api import *
 
 # Mobile
 #from uvcsite.mobile import BaseMobilePage, MobilePage, MobileLayer, IMobileLayer
-
 
 ### ZEAM-FORM-API
 from zeam.form.base import Fields, Action, Actions, action, DictDataManager
@@ -32,8 +29,7 @@ from zeam.form.base.errors import Error
 ### DOLMEN-FORMS-API
 from dolmen.forms.base.models import ApplicationForm
 from dolmen.forms.base.interfaces import IFieldUpdate
-from dolmen.forms.base.utils import (
-    notify_changes, apply_data_event)
+from dolmen.forms.base.utils import notify_changes, apply_data_event
 from uvcsite.utils.form import set_fields_data
 
 #from uvcsite.utils.mail import send_mail
