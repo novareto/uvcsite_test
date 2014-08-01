b5d1dadc9d5da366cf8e8ad8d4df2acb43cde652
trollfot <trollfot@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2014-08-01 09:44:26 +0000

    Removing use of zope.app, prior to fixing

--- uvcsite/branches/uvc_api_migration/src/uvcsite/homefolder/homefolder.py
+++ uvcsite/branches/uvc_api_migration/src/uvcsite/homefolder/homefolder.py
@@ -2,24 +2,15 @@
 
 ## import grok
 ## import uvcsite
+# import zope.securitypolicy.interfaces
+# import zope.securitypolicy.principalrole
 
 ## from uvcsite.content import IProductFolder
-## from uvcsite.interfaces import IMyHomeFolder, IGetHomeFolderUrl
-## from uvcsite.auth.interfaces import IMasterUser
-
-## from zope.app.homefolder.homefolder import HomeFolderManager
-## from zope.app.homefolder.interfaces import IHomeFolderManager
-## import zope.app.homefolder.homefolder
 ## from zope.component import getUtilitiesFor
 ## from zope.securitypolicy.interfaces import IPrincipalRoleManager
 ## from zope.dottedname.resolve import resolve
 ## from zope.security.interfaces import IPrincipal
-## from zope.app.security.interfaces import IUnauthenticatedPrincipal
-## from zope.app.homefolder.interfaces import IHomeFolder
-## from zope.publisher.interfaces.browser import IBrowserRequest
 
-# import zope.securitypolicy.interfaces
-# import zope.securitypolicy.principalrole
 
 # class PrincipalRoleManager(
 #     zope.securitypolicy.principalrole.AnnotationPrincipalRoleManager,
