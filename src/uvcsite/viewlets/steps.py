d3b2a0a0a734eebbfae52bb8b262dc9e0fa15272
ck <ck@novareto.de>
2014-11-27 09:28:59 +0000

    chagnes steps

--- src/uvcsite/viewlets/steps.py
+++ src/uvcsite/viewlets/steps.py
@@ -15,26 +15,3 @@ class StepsProgressBar(grok.Viewlet):
     grok.view(uvcsite.Wizard)
     grok.viewletmanager(uvcsite.IAboveContent)
     grok.order(10000)
-
-    current = "current"
-    past = "past"
-    future = "future"
-
-    def update(self):
-        self.steps = []
-        current = self.view.step
-        subforms = self.view._getAvailableSubForms()
-        self.title = "Fortschrittsanzeige: %s von %s" %(current+1, len(subforms))
-        for i, step in enumerate(subforms):
-            link = ''
-            if i == current:
-                css = self.current
-                self.title = "%s - %s  +" %(self.title, step.label)
-            elif i > current:
-                css = self.future
-            elif i < current:
-                css = self.past
-                link= "%s/edit?jump_step=%s" % (self.view.url(), i)
-            self.steps.append(
-                dict(description = step.label, css=css, link = link)
-                )
