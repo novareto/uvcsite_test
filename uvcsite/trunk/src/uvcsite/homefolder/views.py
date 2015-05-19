9ba05d98a7490de42d35264b74f19f9b790bf8a1
ck@novareto.de <ck@novareto.de@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2015-05-19 07:39:42 +0000

    views

--- uvcsite/trunk/src/uvcsite/homefolder/views.py
+++ uvcsite/trunk/src/uvcsite/homefolder/views.py
@@ -67,6 +67,21 @@ class Index(TablePage):
         super(Index, self).update()
 
 
+    def renderCell(self, item, column, colspan=0):
+        from z3c.table import interfaces
+        if interfaces.INoneCell.providedBy(column):
+            return u''
+        cssClass = column.cssClasses.get('td')
+        cssClass = self.getCSSHighlightClass(column, item, cssClass)
+        cssClass = self.getCSSSortClass(column, cssClass)
+        cssClass = self.getCSSClass('td', cssClass)
+        colspanStr = colspan and ' colspan="%s"' % colspan or ''
+        dt = ' data-title="%s" ' % column.id
+        return u'\n      <td%s%s%s>%s</td>' % (cssClass, colspanStr, dt,
+            column.renderCell(item))
+
+
+
 class DirectAccessViewlet(grok.Viewlet):
     grok.view(IFolderListingTable)
     grok.order(25)
