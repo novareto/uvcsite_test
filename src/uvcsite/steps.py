c59155966ea3385f4604d61743984d66597420bb
Christian Klinger <ck@novareto.de>
2015-10-07 10:03:14 +0200

    changes

---
+++ src/uvcsite/steps.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2010 NovaReto GmbH
+# cklinger@novareto.de
+
+import grok
+import uvcsite
+from zope.interface import Interface
+
+
+grok.templatedir('templates')
+
+
+class StepsProgressBar(grok.Viewlet):
+    grok.context(Interface)
+    grok.view(uvcsite.Wizard)
+    grok.viewletmanager(uvcsite.IAboveContent)
+    grok.order(10000)
+    grok.require('zope.View')
+
