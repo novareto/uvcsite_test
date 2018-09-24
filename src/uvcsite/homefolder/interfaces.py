ae023670b99f19e8aa69235428ef28128b8e5723
Christian Klinger <ck@novareto.de>
2018-09-24 14:47:59 +0200

    add interfaces

---
+++ src/uvcsite/homefolder/interfaces.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# # Copyright (c) 2007-2013 NovaReto GmbH
+# # cklinger@novareto.de 
+
+from zope.interface import Interface
+
+class IHomeFolderManager(Interface):
+    """ """
+
+class IHomeFolder(Interface):
+    """ """
